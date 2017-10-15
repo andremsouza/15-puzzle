1fb7cb626b6c66701610a16b13236b97faffdcf2
andremsouza <msouza.andre@hotmail.com>
2017-10-15 19:45:26 -0200

    Two files. Each one with a different algorithm.

---
+++ aStar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int N; cin >> N;
+    while(N--){
+        vvi grid(4, vi(4));
+        for(int i=0;i<4;i++)
+            for(int j=0;j<4;j++)
+                cin >> grid[i][j];
+    }
+    return 0;
+}
